49b7dace8e3f080dceae821da0a5413abef0a5cc
Đỗ TRọng Hoàng <77737898+dotronghoang2004@users.noreply.github.com>
2022-08-21 21:50:35 +0700

    upload

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+function hideFire (){
+    let e = document.getElementById('candle_fire_1');
+    let j = document.getElementById('candle_fire_2');
+
+    e.classList.add("mystyle");
+    j.classList.add("mystyle");
+    
+}
